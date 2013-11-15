1962f4a2327d89142cfc15fbe448c2d55fe941e9
Joshua Harlow <harlowja@yahoo-inc.com>
2013-11-15 09:43:08 -0800

    Add supporting files
    add rudimentary documentation skeleton
    
    Change-Id: I16cb41e72ed6ae296e574ff4973a5ab28d49a100

--- tomograph/__init__.py
+++ tomograph/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 Yahoo! Inc. All rights reserved.  
+# Copyright (c) 2012 Yahoo! Inc. All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License"); you
 # may not use this file except in compliance with the License. You may
 # obtain a copy of the License at
@@ -12,28 +12,32 @@
 ### Initialize logging in case it hasn't been done.  We need two
 ### versions of this, one for the eventlet logging module and one for
 ### the non-eventlet one...
+
+from __future__ import absolute_import
+
 import logging
 import sys
+
 import eventlet
 eventlet_logging = eventlet.import_patched('logging')
 eventlet_sys = eventlet.import_patched('sys')
 
+
 def _initLogging(logging, sys):
-    """
-    set up some default stuff, in case nobody configured logging yet
-    """
+    """Set up some default stuff, in case nobody configured logging yet."""
     logger = logging.getLogger('tomograph')
 
     if logger.level == logging.NOTSET:
         logger.setLevel(logging.INFO)
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s %(name)s %(message)s'))
+        handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s '
+                                               '%(name)s %(message)s'))
         logger.addHandler(handler)
+
 
 _initLogging(logging, sys)
 _initLogging(eventlet_logging, eventlet_sys)
 
-import config
-from tomograph import *
+
+from tomograph.tomograph import *
